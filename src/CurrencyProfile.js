f936400b91aa00d55c24d1e7958b408fd1b36b72
Prabhat Sahrawat <prabhatsahrawat010203@gmail.com>
2022-09-07 17:53:20 +0000

    currency  profile

---
+++ src/CurrencyProfile.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import react from "react";
+import ReactDOM from "react-dom";
+import cont from "../public/images/country.jpg";
+export default function CurrencyProfile() {
+  return (
+    <section>
+      <div className="currency">
+        <h1 className="crr1">Daily market updates straight to your inbox</h1>
+        <button className="btn1"> Sign Up</button>
+      </div>
+      <div>
+        <h1 className="crr1">Currency Profiles</h1>
+        <img className="star" src={cont} />
+      </div>
+    </section>
+  );
+}
